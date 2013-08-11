e1b1bc7117c3cc0aff1461aeee3886fd44195440
Fredy Wijaya <fredy.wijaya@gmail.com>
2013-08-11 17:57:55 -0500

    Implement heap sort

--- src/test/java/algorithms/PriorityQueueTest.java
+++ src/test/java/algorithms/PriorityQueueTest.java
@@ -7,33 +7,33 @@ import org.junit.Test;
 public class PriorityQueueTest {
     @Test
     public void testDescending() {
-        Integer[] a = new Integer[]{5, 8, 6, 3, 2, 1, 4, 7, 9};
+        Integer[] a = new Integer[]{10, 5, 8, 6, 3, 2, 1, 4, 7, 9};
         PriorityQueue<Integer> pq = new PriorityQueue<>(true);
         for (int i : a) {
             pq.insert(i);
         }
-        assertEquals(9, pq.size());
+        assertEquals(10, pq.size());
         Integer[] result = new Integer[pq.size()];
         for (int i = 0; i < result.length; i++) {
             result[i] = pq.remove();
         }
-        assertEquals("1 2 3 4 5 6 7 8 9", join(result, " "));
+        assertEquals("1 2 3 4 5 6 7 8 9 10", join(result, " "));
         assertEquals(0, pq.size());
     }
     
     @Test
     public void testAscending() {
-        Integer[] a = new Integer[]{5, 8, 6, 3, 2, 1, 4, 7, 9};
+        Integer[] a = new Integer[]{10, 5, 8, 6, 3, 2, 1, 4, 7, 9};
         PriorityQueue<Integer> pq = new PriorityQueue<>(false);
         for (int i : a) {
             pq.insert(i);
         }
-        assertEquals(9, pq.size());
+        assertEquals(10, pq.size());
         Integer[] result = new Integer[pq.size()];
         for (int i = 0; i < result.length; i++) {
             result[i] = pq.remove();
         }
-        assertEquals("9 8 7 6 5 4 3 2 1", join(result, " "));
+        assertEquals("10 9 8 7 6 5 4 3 2 1", join(result, " "));
         assertEquals(0, pq.size());
     }
     
