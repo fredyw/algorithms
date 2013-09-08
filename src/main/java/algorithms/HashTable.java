9f7ccdee7dda59bb11ebdce09587f426bb62b7ca
Fredy Wijaya <fredy.wijaya@gmail.com>
2013-09-08 13:52:58 -0500

    Implement hash table with separate chaining

---
+++ src/main/java/algorithms/HashTable.java
@@ -0,0 +1,62 @@
+package algorithms;
+
+import java.util.LinkedList;
+
+public class HashTable<Key, Value> {
+    private class KeyValue {
+        private Key key;
+        private Value value;
+        
+        public KeyValue(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    private LinkedList<KeyValue>[] keyValues;
+    private int n;
+    private int size;
+    
+    public HashTable() {
+        this(997);
+    }
+    
+    @SuppressWarnings("unchecked")
+    private HashTable(int n) {
+        this.n = n;
+        this.keyValues = new LinkedList[n];
+        for (int i = 0; i < keyValues.length; i++) {
+            keyValues[i] = new LinkedList<KeyValue>();
+        }
+    }
+    
+    public void put(Key key, Value value) {
+        LinkedList<KeyValue> ll = keyValues[getIndex(key)];
+        for (KeyValue kv : ll) {
+            if (kv.key.equals(key)) {
+                kv.value = value;
+                return;
+            }
+        }
+        size++;
+        ll.addFirst(new KeyValue(key, value));
+    }
+    
+    public Value get(Key key) {
+        LinkedList<KeyValue> ll = keyValues[getIndex(key)];
+        for (KeyValue kv : ll) {
+            if (kv.key.equals(key)) {
+                return kv.value;
+            }
+        }
+        return null;
+    }
+    
+    private int getIndex(Key key) {
+        return (key.hashCode() & 0x7fffffff) % n;
+    }
+    
+    public int getSize() {
+        return size;
+    }
+}
---
+++ src/test/java/algorithms/HashTableTest.java
@@ -0,0 +1,33 @@
+package algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HashTableTest {
+    @Test
+    public void test() {
+        HashTable<String, Integer> ht = new HashTable<>();
+        ht.put("e", 1);
+        ht.put("b", 2);
+        ht.put("c", 3);
+        ht.put("c", 30);
+        ht.put("d", 4);
+        ht.put("a", 5);
+        ht.put("h", 6);
+        ht.put("f", 7);
+        ht.put("g", 8);
+        
+        assertEquals(new Integer(1), ht.get("e"));
+        assertEquals(new Integer(2), ht.get("b"));
+        assertEquals(new Integer(30), ht.get("c"));
+        assertEquals(new Integer(4), ht.get("d"));
+        assertEquals(new Integer(5), ht.get("a"));
+        assertEquals(new Integer(6), ht.get("h"));
+        assertEquals(new Integer(7), ht.get("f"));
+        assertEquals(new Integer(8), ht.get("g"));
+        
+        assertEquals(8, ht.getSize());
+    }
+
+}
