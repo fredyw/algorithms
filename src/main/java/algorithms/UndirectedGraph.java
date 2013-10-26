aa812d6b153c4ce144720627171160bda67a9b7c
Fredy Wijaya <fredy.wijaya@gmail.com>
2013-10-25 21:55:09 -0500

    Implement DFS and BFS for undirected graph

--- src/main/java/algorithms/UndirectedGraph.java
+++ src/main/java/algorithms/UndirectedGraph.java
@@ -1,7 +1,10 @@
 package algorithms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -34,6 +37,89 @@ public class UndirectedGraph<T> {
             vertices.put(node2, newSet);
         } else {
             vertices.get(node2).add(node1);
+        }
+    }
+    
+    public static class DepthFirstSearch<T> {
+        private Set<T> marked = new HashSet<>();
+        private Map<T, T> edgeTo = new HashMap<>();
+        
+        public DepthFirstSearch(UndirectedGraph<T> graph, T source) {
+            dfs(graph, source);
+        }
+        
+        private void dfs(UndirectedGraph<T> graph, T source) {
+            marked.add(source);
+            for (T adj : graph.adjacent(source)) {
+                if (!marked.contains(adj)) {
+                    edgeTo.put(adj, source);
+                    dfs(graph, adj);
+                }
+            }
+        }
+        
+        public boolean hasPathTo(T target) {
+            return marked.contains(target);
+        }
+        
+        public List<T> pathTo(T target) {
+            if (!hasPathTo(target)) {
+                return new ArrayList<T>();
+            }
+            List<T> paths = new ArrayList<>();
+            paths.add(target);
+            T t = edgeTo.get(target);
+            while (t != null) {
+                paths.add(t);
+                t = edgeTo.get(t);
+            }
+            return paths;
+        }
+    }
+    
+    public static class BreadthFirstSearch<T> {
+        Set<T> marked = new HashSet<>();
+        private Map<T, T> edgeTo = new HashMap<>();
+        
+        public BreadthFirstSearch(UndirectedGraph<T> graph, T source) {
+            bfs(graph, source);
+        }
+        
+        private void bfs(UndirectedGraph<T> graph, T source) {
+            T s = source;
+            LinkedList<T> nodes = new LinkedList<>();
+            while (true) {
+                marked.add(s);
+                for (T adj : graph.adjacent(s)) {
+                    if (!marked.contains(adj)) {
+                        nodes.add(adj);
+                        marked.add(adj);
+                        edgeTo.put(adj, s);
+                    }
+                }
+                if (nodes.size() == 0) {
+                    break;
+                }
+                s = nodes.removeFirst();
+            }
+        }
+        
+        public boolean hasPathTo(T target) {
+            return marked.contains(target);
+        }
+        
+        public List<T> pathTo(T target) {
+            if (!hasPathTo(target)) {
+                return new ArrayList<T>();
+            }
+            List<T> paths = new ArrayList<>();
+            paths.add(target);
+            T t = edgeTo.get(target);
+            while (t != null) {
+                paths.add(t);
+                t = edgeTo.get(t);
+            }
+            return paths;
         }
     }
 }
--- src/test/java/algorithms/UndirectedGraphTest.java
+++ src/test/java/algorithms/UndirectedGraphTest.java
@@ -6,6 +6,9 @@ import java.util.Set;
 
 import org.junit.Test;
 
+import algorithms.UndirectedGraph.BreadthFirstSearch;
+import algorithms.UndirectedGraph.DepthFirstSearch;
+
 public class UndirectedGraphTest {
     @Test
     public void test() {
@@ -30,5 +33,53 @@ public class UndirectedGraphTest {
        assertTrue(adj.contains(2));
        assertTrue(adj.contains(5));
        assertTrue(adj.contains(7));
+    }
+    
+    @Test
+    public void testDepthFirstSearch() {
+       UndirectedGraph<Integer> graph = new UndirectedGraph<>();
+       graph.add(0, 5);
+       graph.add(2, 4);
+       graph.add(2, 3);
+       graph.add(1, 2);
+       graph.add(0, 1);
+       graph.add(3, 4);
+       graph.add(3, 5);
+       graph.add(0, 2);
+       
+       DepthFirstSearch<Integer> dfs = new DepthFirstSearch<>(graph, 0);
+       assertTrue(dfs.hasPathTo(5));
+       assertFalse(dfs.hasPathTo(10));
+       assertEquals("[5, 3, 2, 1, 0]", dfs.pathTo(5).toString());
+       assertEquals("[4, 3, 2, 1, 0]", dfs.pathTo(4).toString());
+       assertEquals("[3, 2, 1, 0]", dfs.pathTo(3).toString());
+       assertEquals("[2, 1, 0]", dfs.pathTo(2).toString());
+       assertEquals("[1, 0]", dfs.pathTo(1).toString());
+       assertEquals("[0]", dfs.pathTo(0).toString());
+       assertEquals("[]", dfs.pathTo(10).toString());
+    }
+    
+    @Test
+    public void testBreadthFirstSearch() {
+       UndirectedGraph<Integer> graph = new UndirectedGraph<>();
+       graph.add(0, 5);
+       graph.add(2, 4);
+       graph.add(2, 3);
+       graph.add(1, 2);
+       graph.add(0, 1);
+       graph.add(3, 4);
+       graph.add(3, 5);
+       graph.add(0, 2);
+       
+       BreadthFirstSearch<Integer> bfs = new BreadthFirstSearch<>(graph, 0);
+       assertTrue(bfs.hasPathTo(5));
+       assertFalse(bfs.hasPathTo(10));
+       assertEquals("[5, 0]", bfs.pathTo(5).toString());
+       assertEquals("[4, 2, 0]", bfs.pathTo(4).toString());
+       assertEquals("[3, 2, 0]", bfs.pathTo(3).toString());
+       assertEquals("[2, 0]", bfs.pathTo(2).toString());
+       assertEquals("[1, 0]", bfs.pathTo(1).toString());
+       assertEquals("[0]", bfs.pathTo(0).toString());
+       assertEquals("[]", bfs.pathTo(10).toString());
     }
 }
