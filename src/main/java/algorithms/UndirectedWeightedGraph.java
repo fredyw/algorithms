3d5247b1694bd83be036008e59b46c17627120a3
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-02-04 18:16:47 -0600

    Implement minimum spanning tree using Prim's algorithm

--- src/main/java/algorithms/UndirectedWeightedGraph.java
+++ src/main/java/algorithms/UndirectedWeightedGraph.java
@@ -1,42 +1,52 @@
 package algorithms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 public class UndirectedWeightedGraph<T> {
-    private Map<Vertex<T>, Set<Vertex<T>>> vertices = new HashMap<>();
+    private Map<T, Set<Edge<T>>> vertices = new HashMap<>();
     
-    public static class Vertex<T> {
-        private int weight;
-        private T t;
+    public static class Edge<T> implements Comparable<T> {
+        private double weight;
+        private T t1;
+        private T t2;
         
-        public Vertex(T t, int weight) {
-            this.t = t;
+        public Edge(T t1, T t2, double weight) {
+            this.t1 = t1;
+            this.t2 = t2;
             this.weight = weight;
         }
-        
-        public Vertex(T t) {
-            this(t, 0);
-        }
 
-        public T getVertex() {
-            return t;
+        public T either() {
+            return t1;
         }
         
-        public int getWeight() {
+        public T other(T t) {
+            if (!t.equals(t1)) {
+                return t1;
+            }
+            return t2;
+        }
+        
+        public double getWeight() {
             return weight;
         }
-        
+
         @Override
         public int hashCode() {
             final int prime = 31;
             int result = 1;
-            result = prime * result + ((t == null) ? 0 : t.hashCode());
+            result = prime * result + ((t1 == null) ? 0 : t1.hashCode());
+            result = prime * result + ((t2 == null) ? 0 : t2.hashCode());
             return result;
         }
 
+        @SuppressWarnings("unchecked")
         @Override
         public boolean equals(Object obj) {
             if (this == obj) {
@@ -48,44 +58,119 @@ public class UndirectedWeightedGraph<T> {
             if (getClass() != obj.getClass()) {
                 return false;
             }
-            Vertex<?> other = (Vertex<?>) obj;
-            if (t == null) {
-                if (other.t != null) {
+            Edge<T> other = (Edge<T>) obj;
+            if (t1 == null) {
+                if (other.t1 != null) {
                     return false;
                 }
-            } else if (!t.equals(other.t)) {
+            } else if (!t1.equals(other.t1)) {
+                return false;
+            }
+            if (t2 == null) {
+                if (other.t2 != null) {
+                    return false;
+                }
+            } else if (!t2.equals(other.t2)) {
                 return false;
             }
             return true;
         }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public int compareTo(T o) {
+            return Double.valueOf(weight).compareTo(((Edge<T>) o).weight);
+        }
+
+        /**
+         * {@inheritDoc} 
+         */
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append(t1).append("-").append(t2).append(" ").append(weight);
+            return builder.toString();
+        }
     }
     
-    public Set<Vertex<T>> getVertices() {
+    public Set<T> getVertices() {
         return vertices.keySet();
     }
     
-    public Set<Vertex<T>> adjacent(Vertex<T> vertex) {
-        if (!vertices.containsKey(vertex)) {
+    public Set<Edge<T>> adjacent(T t) {
+        if (!vertices.containsKey(t)) {
             return new HashSet<>();
         }
-        return vertices.get(vertex);
+        return vertices.get(t);
     }
     
-    public void add(Vertex<T> vertex1, Vertex<T> vertex2) {
-        if (!vertices.containsKey(vertex1)) {
-            Set<Vertex<T>> newSet = new HashSet<>();
-            newSet.add(vertex2);
-            vertices.put(vertex1, newSet);
+    public void add(Edge<T> edge) {
+        T t1 = edge.either();
+        if (!vertices.containsKey(t1)) {
+            Set<Edge<T>> newSet = new HashSet<>();
+            newSet.add(edge);
+            vertices.put(t1, newSet);
         } else {
-            vertices.get(vertex1).add(vertex2);
+            vertices.get(t1).add(edge);
         }
         
-        if (!vertices.containsKey(vertex2)) {
-            Set<Vertex<T>> newSet = new HashSet<>();
-            newSet.add(vertex1);
-            vertices.put(vertex2, newSet);
+        T t2 = edge.other(t1);
+        if (!vertices.containsKey(t2)) {
+            Set<Edge<T>> newSet = new HashSet<>();
+            newSet.add(edge);
+            vertices.put(t2, newSet);
         } else {
-            vertices.get(vertex2).add(vertex1);
+            vertices.get(t2).add(edge);
+        }
+    }
+    
+    public static class MinimumSpanningTree<T> {
+        private Set<T> marked = new HashSet<>();
+        private PriorityQueue<Edge<T>> pq = new PriorityQueue<>();
+        private List<Edge<T>> mst = new ArrayList<>();
+        
+        public MinimumSpanningTree(UndirectedWeightedGraph<T> graph) {
+            if (graph.getVertices().size() == 0) {
+                return;
+            }
+            T t = graph.getVertices().iterator().next();
+            visit(graph, t);
+            while (!pq.isEmpty()) {
+                Edge<T> edge = pq.remove();
+                T t1 = edge.either();
+                T t2 = edge.other(t1);
+                if (marked.contains(t1) && marked.contains(t2)) {
+                    continue;
+                }
+                mst.add(edge);
+                if (!marked.contains(t1)) {
+                    visit(graph, t1);
+                }
+                if (!marked.contains(t2)) {
+                    visit(graph, t2);
+                }
+            }
+        }
+        
+        private void visit(UndirectedWeightedGraph<T> graph, T t) {
+            marked.add(t);
+            for (Edge<T> e : graph.adjacent(t)) {
+                if (!marked.contains(e.other(t))) {
+                    pq.add(e);
+                }
+            }
+        }
+        
+        public List<Edge<T>> getPaths() {
+            return mst;
+        }
+        
+        public double getWeight() {
+            double weight = 0;
+            for (Edge<T> e : mst) {
+                weight += e.getWeight();
+            }
+            return weight;
         }
     }
 }
--- src/test/java/algorithms/UndirectedWeightedGraphTest.java
+++ src/test/java/algorithms/UndirectedWeightedGraphTest.java
@@ -6,31 +6,59 @@ import java.util.Set;
 
 import org.junit.Test;
 
-import algorithms.UndirectedWeightedGraph.Vertex;
+import algorithms.UndirectedWeightedGraph.Edge;
+import algorithms.UndirectedWeightedGraph.MinimumSpanningTree;
 
 public class UndirectedWeightedGraphTest {
     @Test
     public void test() {
         UndirectedWeightedGraph<Integer> graph = new UndirectedWeightedGraph<>();
-        graph.add(new Vertex<>(1), new Vertex<>(2));
-        graph.add(new Vertex<>(2), new Vertex<>(3));
-        graph.add(new Vertex<>(4), new Vertex<>(1));
-        graph.add(new Vertex<>(5), new Vertex<>(3));
-        graph.add(new Vertex<>(6), new Vertex<>(1));
-        graph.add(new Vertex<>(3), new Vertex<>(7));
+        graph.add(new Edge<>(1, 2, 0));
+        graph.add(new Edge<>(2, 3, 0));
+        graph.add(new Edge<>(4, 1, 0));
+        graph.add(new Edge<>(5, 3, 0));
+        graph.add(new Edge<>(6, 1, 0));
+        graph.add(new Edge<>(3, 7, 0));
         
         assertEquals(7, graph.getVertices().size());
-        assertTrue(graph.adjacent(new Vertex<>(10)).isEmpty());
-        Set<Vertex<Integer>> adj = graph.adjacent(new Vertex<>(1));
+        assertTrue(graph.adjacent(10).isEmpty());
+        Set<Edge<Integer>> adj = graph.adjacent(1);
         assertEquals(3, adj.size());
-        assertTrue(adj.contains(new Vertex<>(6)));
-        assertTrue(adj.contains(new Vertex<>(4)));
-        assertTrue(adj.contains(new Vertex<>(2)));
+        assertTrue(adj.contains(new Edge<>(6, 1, 0)));
+        assertTrue(adj.contains(new Edge<>(4, 1, 0)));
+        assertTrue(adj.contains(new Edge<>(1, 2, 0)));
         
-        adj = graph.adjacent(new Vertex<>(3));
+        adj = graph.adjacent(3);
         assertEquals(3, adj.size());
-        assertTrue(adj.contains(new Vertex<>(2)));
-        assertTrue(adj.contains(new Vertex<>(5)));
-        assertTrue(adj.contains(new Vertex<>(7)));
+        assertTrue(adj.contains(new Edge<>(2, 3, 0)));
+        assertTrue(adj.contains(new Edge<>(5, 3, 0)));
+        assertTrue(adj.contains(new Edge<>(3, 7, 0)));
+    }
+    
+    @Test
+    public void testMinimumSpanningTree() {
+        UndirectedWeightedGraph<Integer> graph = new UndirectedWeightedGraph<>();
+        graph.add(new Edge<>(4, 5, 0.35));
+        graph.add(new Edge<>(4, 7, 0.37));
+        graph.add(new Edge<>(5, 7, 0.28));
+        graph.add(new Edge<>(0, 7, 0.16));
+        graph.add(new Edge<>(1, 5, 0.32));
+        graph.add(new Edge<>(0, 4, 0.38));
+        graph.add(new Edge<>(2, 3, 0.17));
+        graph.add(new Edge<>(1, 7, 0.19));
+        graph.add(new Edge<>(0, 2, 0.26));
+        graph.add(new Edge<>(1, 2, 0.36));
+        graph.add(new Edge<>(1, 3, 0.29));
+        graph.add(new Edge<>(2, 7, 0.34));
+        graph.add(new Edge<>(6, 2, 0.40));
+        graph.add(new Edge<>(3, 6, 0.52));
+        graph.add(new Edge<>(6, 0, 0.58));
+        graph.add(new Edge<>(6, 4, 0.93));
+        
+        MinimumSpanningTree<Integer> mst = new MinimumSpanningTree<>(graph);
+        assertEquals(
+            "[0-7 0.16, 1-7 0.19, 0-2 0.26, 2-3 0.17, 5-7 0.28, 4-5 0.35, 6-2 0.4]",
+            mst.getPaths().toString());
+        assertEquals(1.81, mst.getWeight(), 0.0);
     }
 }
