b0cb6d221f8e548b597140e9e0380a16d1aaf64f
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-10-27 15:44:06 -0500

    Add a skeleton of Rabin Karp substring search algorithm

---
+++ src/main/java/algorithms/RabinKarpSubstringSearch.java
@@ -0,0 +1,8 @@
+package algorithms;
+
+public class RabinKarpSubstringSearch {
+    public static int substring(String pattern, String string) {
+        // TODO: to be implemented
+        return -1;
+    }
+}
---
+++ src/test/java/algorithms/RabinKarpSubstringSearchTest.java
@@ -0,0 +1,18 @@
+package algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RabinKarpSubstringSearchTest {
+    @Test
+    public void test() {
+        assertEquals(0, SubstringSearch.substring("hello", "hello world"));
+        assertEquals(2, SubstringSearch.substring("llo", "hello world"));
+        assertEquals(5, SubstringSearch.substring(" ", "hello world"));
+        assertEquals(0, SubstringSearch.substring("hello world", "hello world"));
+        assertEquals(-1, SubstringSearch.substring("word", "hello world"));
+        assertEquals(-1, SubstringSearch.substring("foo", "hello world"));
+        assertEquals(-1, SubstringSearch.substring("hello world1", "hello world"));
+    }
+}
