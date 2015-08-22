03149224525ab3d815cc8b2a6d184fea870e9b9e
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-21 19:48:54 -0500

    Add counting sort

---
+++ src/main/java/algorithms/CountingSort.java
@@ -0,0 +1,23 @@
+package algorithms;
+
+public class CountingSort {
+    public static void sort(int[] array, int size) {
+        int[] aux = new int[array.length];
+        for (int i = 0; i < aux.length; i++) {
+            aux[i] = array[i];
+        }
+        int[] count = new int[size];
+        for (int i = 0; i < aux.length; i++) {
+            count[aux[i]] += 1;
+        }
+        for (int i = 1; i < count.length; i++) {
+            count[i] = count[i-1] + count[i];
+        }
+        for (int i = aux.length-1; i >= 0; i--) {
+            int val = aux[i];
+            count[val]--;
+            int idx = count[val];
+            array[idx] = val;
+        }
+    }
+}
---
+++ src/test/java/algorithms/CountingSortTest.java
@@ -0,0 +1,20 @@
+package algorithms;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class CountingSortTest {
+    @Test
+    public void testSort() {
+        int[] array = new int[]{3, 2, 1, 2, 3, 1};
+        CountingSort.sort(array, 20);
+        assertEquals("[1, 1, 2, 2, 3, 3]", Arrays.toString(array));
+        
+        array = new int[]{1, 4, 1, 2, 7, 5, 2};
+        CountingSort.sort(array, 20);
+        assertEquals("[1, 1, 2, 2, 4, 5, 7]", Arrays.toString(array));
+    }
+}
