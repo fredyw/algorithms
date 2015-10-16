1089033b60572c3123b302e9240d217d101f7da0
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-16 00:23:16 -0500

    Update Rabin Karp substring unit test

--- src/test/java/algorithms/RabinKarpSubstringSearchTest.java
+++ src/test/java/algorithms/RabinKarpSubstringSearchTest.java
@@ -7,13 +7,13 @@ import org.junit.Test;
 public class RabinKarpSubstringSearchTest {
     @Test
     public void test() {
-        assertEquals(1, SubstringSearch.substring("hel", "hhelo"));
-        assertEquals(0, SubstringSearch.substring("hello", "hello world"));
-        assertEquals(2, SubstringSearch.substring("llo", "hello world"));
-        assertEquals(5, SubstringSearch.substring(" ", "hello world"));
-        assertEquals(0, SubstringSearch.substring("hello world", "hello world"));
-        assertEquals(-1, SubstringSearch.substring("word", "hello world"));
-        assertEquals(-1, SubstringSearch.substring("foo", "hello world"));
-        assertEquals(-1, SubstringSearch.substring("hello world1", "hello world"));
+        assertEquals(1, RabinKarpSubstringSearch.substring("hel", "hhelo"));
+        assertEquals(0, RabinKarpSubstringSearch.substring("hello", "hello world"));
+        assertEquals(2, RabinKarpSubstringSearch.substring("llo", "hello world"));
+        assertEquals(5, RabinKarpSubstringSearch.substring(" ", "hello world"));
+        assertEquals(0, RabinKarpSubstringSearch.substring("hello world", "hello world"));
+        assertEquals(-1, RabinKarpSubstringSearch.substring("word", "hello world"));
+        assertEquals(-1, RabinKarpSubstringSearch.substring("foo", "hello world"));
+        assertEquals(-1, RabinKarpSubstringSearch.substring("hello world1", "hello world"));
     }
 }
