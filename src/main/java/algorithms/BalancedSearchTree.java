7ac9292c327eef909d068606aa60197899219c5d
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-06 00:02:45 -0500

    Remove redundant static in enum

--- src/main/java/algorithms/BalancedSearchTree.java
+++ src/main/java/algorithms/BalancedSearchTree.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BalancedSearchTree<Key extends Comparable<Key>, Value> {
-    private static enum Color {
+    private enum Color {
         RED, BLACK
     }
 
