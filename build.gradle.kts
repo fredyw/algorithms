dc61004060926ec55d056b439e63f4965ce5dddc
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-01 01:06:47 -0500

    Update Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    java
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
